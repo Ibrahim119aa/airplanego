a5c1a4ed4ed456984f5231b8f8364996f225b884
Ibrahim119aa <ibrahimmemon1709@gmail.com>
2025-07-03 18:07:41 +0500

    New Changs

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Airplane go',
   description: 'Created with v0',
-  generator: 'v0.dev',
+  generator: 'Airplane go',
 }
 
 export default function RootLayout({
